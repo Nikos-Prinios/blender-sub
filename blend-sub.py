935462433fb489da12df650bc71e58ea8f6ad8da
Nikos <habanita@gmail.com>
2015-06-01 09:23:37 +0200

    snap and other things

--- blend-sub.py
+++ blend-sub.py
@@ -12,7 +12,7 @@ bl_info = {
 	"tracker_url": "",}
 ''' version 0.1b  -- not fully operational yet '''
 ''' ------------------------------------------------------------------------------'''
-'''                      IMPORT / PROPERTIES / VARIABLES                          '''
+'''					  IMPORT / PROPERTIES / VARIABLES						  '''
 ''' ------------------------------------------------------------------------------'''
 import bpy, random
 from bpy.app.handlers import persistent
@@ -27,8 +27,49 @@ framerate = 24 #bpy.context.scene.render.fps
 adding_sub = False
 
 ''' ------------------------------------------------------------------------------'''
-'''                                 FUNCTIONS                                     '''
+'''								 FUNCTIONS									 '''
 ''' ------------------------------------------------------------------------------'''
+
+# Bake the vu-meter
+def refresh():
+	path = ''
+	original_type = bpy.context.area.type
+	bpy.context.area.type = "SEQUENCE_EDITOR"
+	for s in bpy.context.scene.sequence_editor.sequences:
+		if s.type == 'SOUND':
+			path = bpy.path.abspath(s.filepath) 
+			pass
+	if exists('vu') and path != '' :
+		bpy.context.area.type = "VIEW_3D";
+		bpy.ops.object.select_all(action='DESELECT')	 
+		vu = bpy.data.objects['vu']
+		bpy.ops.object.select_pattern(pattern="vu")
+		bpy.ops.anim.keyframe_insert_menu(type = "Scaling")
+		vu.animation_data.action.fcurves[0].lock = True
+		vu.animation_data.action.fcurves[2].lock = True
+		bpy.context.area.type = "GRAPH_EDITOR";
+		try: bpy.ops.graph.sound_bake(filepath=path, low=(40), high=(1500))
+		except: print('Sorry, failed to analyse the sound sequence.')
+		bpy.context.area.type = "VIEW_3D";
+		bpy.context.scene.objects.active = vu
+		bpy.ops.object.select_pattern(pattern="vu")  
+		vu.animation_data.action.fcurves[0].lock = False
+		vu.animation_data.action.fcurves[2].lock = False 
+	bpy.context.area.type  = original_type 
+
+# snap to sound peak around
+def snap_to(frame, type):
+	vu = bpy.data.objects['vu']
+	temp = []
+	x = int(framerate)
+	range_from = frame - x
+	for f in range(range_from,frame):
+		try : temp.append([vu.animation_data.action.fcurves[1].evaluate(f),f])
+		except : pass
+	if type == 'start' : temp.sort(reverse=True)
+	else : temp.sort()
+	if len(temp) > 0 : return temp[0][1]
+	else : return frame
 
 def strip_list():
 	global strips, List
@@ -42,8 +83,6 @@ def strip_list():
 			try: 
 				s.name = List[i]
 			except: s.name = ''
-
-
 
 def update_caption_list():
 	global List
@@ -83,7 +122,37 @@ def setup():
 			bpy.context.scene.sequence_editor.active_strip = s
 			s.show_waveform = True
 			bpy.ops.sequencer.view_selected()
+	
+	scene = bpy.context.scene.name
+	bpy.ops.sequencer.select_all(action = "DESELECT")
+	for s in bpy.context.scene.sequence_editor.sequences:
+		if s.name == 'mask' :
+			s.select = True
+		if s.type == 'SCENE' and s.name == scene :
+			s.select = True
+	bpy.ops.sequencer.delete()
+
+	if 'mask' not in bpy.context.scene.sequence_editor.sequences :
+		bpy.ops.sequencer.select_all(action = "DESELECT")
+		end = bpy.context.scene.frame_end
+		res_y = bpy.data.scenes[0].render.resolution_y
+		offset = res_y - (res_y//6)
+		bpy.ops.sequencer.effect_strip_add(frame_start=1, frame_end=end, channel=3, type='COLOR')
+		mask = bpy.context.scene.sequence_editor.active_strip
+		mask.name = 'mask'
+		mask.use_translation = True
+		mask.transform.offset_y = -offset
+		mask.blend_alpha = 0.4
+		mask.blend_type = 'ALPHA_OVER'
+
+	if scene not in bpy.context.scene.sequence_editor.sequences :
+		bpy.ops.sequencer.select_all(action = "DESELECT")
+		bpy.ops.sequencer.scene_strip_add(frame_start=1, channel=4, scene=scene)
+		s = bpy.context.scene.sequence_editor.active_strip
+		s.blend_type = 'ALPHA_OVER'
+		
 	bpy.context.area.type = original_type
+
 
 def find_sub():
 	global strips
@@ -113,6 +182,8 @@ def new_sub_strip(start):
 	seq.effect_strip_add(frame_start=start, frame_end=start+1, type='COLOR', color=(random.uniform(0.5,1),random.uniform(0.5,1),1), overlap=False)
 	current_strip = bpy.context.scene.sequence_editor.active_strip
 	current_strip.blend_alpha = 0
+	if bpy.context.screen.scene.snap == True:
+		current_strip.frame_final_start = snap_to(start,'start')
 	adding_sub = True
 	return
 
@@ -120,6 +191,8 @@ def end_strip(frame):
 	global current_strip, adding_sub
 	current_strip.frame_final_end = frame - 1
 	adding_sub = False
+	if bpy.context.screen.scene.snap == True:
+			current_strip.frame_final_end = snap_to(frame,'start')
 	current_strip = None
 
 # Everything that needs to be done when the frame changes
@@ -130,7 +203,7 @@ def main(self):
 		current_strip.frame_final_end = bpy.context.scene.frame_current + 1
 
 ''' ------------------------------------------------------------------------------'''
-'''                                 INTERFACE                                     '''
+'''								 INTERFACE									 '''
 ''' ------------------------------------------------------------------------------'''
 
 class OBJECT_OT_Setup(bpy.types.Operator):  
@@ -143,9 +216,9 @@ class OBJECT_OT_Setup(bpy.types.Operator):
 		return {'FINISHED'}
 
 class OBJECT_OT_Refresh(bpy.types.Operator):  
-	bl_label = "Refresh"
+	bl_label = "Analyse the sound"
 	bl_idname = "sequencer.refresh"
-	bl_description = "Re-adapt the captions configuration to you sequence"
+	bl_description = "Initialize the vu-meter for snapping feature"
 		
 	def invoke(self, context, event):
 		refresh()
@@ -161,7 +234,7 @@ class OBJECT_OT_Export(bpy.types.Operator):
 		return {'FINISHED'}
 
 class OBJECT_OT_Insert_start(bpy.types.Operator):  
-	bl_label = ""
+	bl_label = "Add"
 	bl_idname = "sequencer.sub_start"
 	bl_description = "start"
 		
@@ -174,13 +247,11 @@ class OBJECT_OT_Insert_start(bpy.types.Operator):
 			end_strip(frame)
 		new_sub_strip(frame)
 		strip_list()
-		'''if bpy.context.screen.scene.snap == True:
-			bpy.ops.marker.move(frames=-(frame - snap_to(frame,'start')))'''
 		bpy.context.area.type = original_type
 		return {'FINISHED'}
 		
 class OBJECT_OT_Insert_end(bpy.types.Operator):  
-	bl_label = ""
+	bl_label = "End"
 	bl_idname = "sequencer.sub_end"
 	bl_description = "end"
 		
@@ -191,10 +262,6 @@ class OBJECT_OT_Insert_end(bpy.types.Operator):
 		frame = bpy.context.scene.frame_current
 		if adding_sub:
 			end_strip(frame)
-		
-		'''if bpy.context.screen.scene.snap == True:
-			frame = bpy.context.scene.frame_current
-			bpy.ops.marker.move(frames=-(frame - snap_to(frame,'end')))'''
 		bpy.context.area.type = original_type
 		return {'FINISHED'}
 
